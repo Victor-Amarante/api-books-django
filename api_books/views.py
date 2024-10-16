d89bc0bc2e20d9e78f497df059af1113b1f5c9f5
Victor Amarante <vafstats@gmail.com>
2024-10-16 15:04:48 -0300

    feat: created view for get books

--- api_books/views.py
+++ api_books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# --- return the books ---
+@api_view(['GET'])
+def get_books(request):
+    books = Book.objects.all()
+    serializerData = BookSerializer(books, many=True).data
+    return Response(serializerData)
+    
